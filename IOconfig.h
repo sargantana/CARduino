1df8d6ca003455afb8be022bd872ebd8664ab4d4
Eric Vauwee <e.vauwee@gmail.com>
2017-11-01 10:11:12 +0100

    Update IOconfig.h

--- IOconfig.h
+++ IOconfig.h
@@ -7,6 +7,4 @@ Pin and IO configuration for RC Car
 #define MOTOR_A_LEFT_PWM 2
 #define MOTOR_B_RIGHT_DIR 3
 #define MOTOR_B_RIGHT_PWM 4
-#define PWM_SLOW 50  
-#define PWM_FAST 200 
-#define DIR_DELAY 1000 
+#define BT_STATE 2
