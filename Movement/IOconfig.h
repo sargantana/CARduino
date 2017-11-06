db6f6b6b9746fa2655eeec41e612a8711640c575
Eric Vauwee <e.vauwee@gmail.com>
2017-11-06 12:55:08 +0100

    Create IOconfig.h

---
+++ Movement/IOconfig.h
@@ -0,0 +1,12 @@
+/* 
+Pin and IO configuration for RC Car
+*/
+
+#define LED 13
+#define MOTOR_A_LEFT_DIR 1
+#define MOTOR_A_LEFT_PWM 2
+#define MOTOR_B_RIGHT_DIR 3
+#define MOTOR_B_RIGHT_PWM 4
+#define BT_STATE 5
+#define TACHO_L 20
+#define TACHO_R 21
